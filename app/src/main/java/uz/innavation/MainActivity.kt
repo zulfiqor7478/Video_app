4a178516955589fb9471b59725e713b6bce54fa7
Zulfiqor <zulfiqorrashidov@gmail.com>
2022-03-04 17:13:03 +0500

    Initial commit

---
+++ app/src/main/java/uz/innavation/MainActivity.kt
@@ -0,0 +1,15 @@
+package uz.innavation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uz.innavation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
