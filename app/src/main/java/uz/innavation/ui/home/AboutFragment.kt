3d1ff8ebccfacb203daf7d9a0457ad04f3cf76f9
Otabek <yakbotabek@gmail.com>
2022-06-01 18:27:29 +0500

    update3

--- app/src/main/java/uz/innavation/ui/home/AboutFragment.kt
+++ app/src/main/java/uz/innavation/ui/home/AboutFragment.kt
@@ -6,9 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import uz.innavation.R
 import uz.innavation.databinding.FragmentAboutBinding
-import uz.innavation.databinding.FragmentHomePageBinding
 
 class AboutFragment : Fragment() {
     lateinit var binding: FragmentAboutBinding
@@ -18,11 +16,17 @@ class AboutFragment : Fragment() {
     ): View {
 
         binding = FragmentAboutBinding.inflate(layoutInflater)
-
         binding.backBtn.setOnClickListener {
             findNavController().popBackStack()
         }
-
+        binding.dlg1.setOnClickListener {
+            val dlg1 = CustomDialogFragment1()
+            dlg1.show(childFragmentManager, "customDiolog")
+        }
+        binding.dlg2.setOnClickListener {
+            val dlg2 = CustomDialogFragment2()
+            dlg2.show(childFragmentManager, "customDiolog")
+        }
         return binding.root
     }
 
