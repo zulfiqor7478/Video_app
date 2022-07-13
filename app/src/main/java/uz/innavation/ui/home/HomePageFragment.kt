4204df97b3db3733024cf8a66a78e566dcd6eed6
Alovuddin <qambarovoloviddin@gmail.com>
2022-07-13 10:16:28 -0700

    finished :)

--- app/src/main/java/uz/innavation/ui/home/HomePageFragment.kt
+++ app/src/main/java/uz/innavation/ui/home/HomePageFragment.kt
@@ -2,11 +2,13 @@ package uz.innavation.ui.home
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import uz.innavation.R
